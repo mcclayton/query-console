3ee2807352cc713345a090350a41efc1f0c7ce14
Michael Clayton <michael.clayton@procore.com>
2018-08-21 17:35:45 -0700

    Remove escape codes from queries

--- src/server/queryTracker.js
+++ src/server/queryTracker.js
@@ -1,7 +1,9 @@
 const Tail = require('tail').Tail;
 const logUtil = require('./logUtil');
 
-const QUERY_REGEX = /^(select|create|update|delete|insert)\b/i;
+const QUERY_REGEX = /^.*(select|create|update|delete|insert)\b/i;
+// For removing color, etc. escape codes from text
+const ESCAPE_CODE_REGEX = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
 
 class QueryTracker {
   constructor(path, service) {
@@ -14,8 +16,9 @@ class QueryTracker {
 
     this.tail.on("line", (data) => {
       if (QUERY_REGEX.test(data)) {
-        console.log(`->> QUERY (${this.service}):`, data);
-        const entry = this._createLogEntry(data);
+        const cleanedData = data.replace(ESCAPE_CODE_REGEX, '');
+        console.log(`->> QUERY (${this.service}):`, cleanedData);
+        const entry = this._createLogEntry(cleanedData);
         logUtil.appendToLog(entry);
       }
     });
