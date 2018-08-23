3e2a892e28e56167e4335404d822a8eea20a270a
Michael Clayton <michael.clayton@procore.com>
2018-08-23 12:34:57 -0700

    Commit all node modules and fix packaging for npm

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Set appropriate working directory
+cd "$(npm root -g)/query-console"
+
 echo "  _____                    _____                 _      "
 echo " |     |_ _ ___ ___ _ _   |     |___ ___ ___ ___| |___  "
 echo " |  |  | | | -_|  _| | |  |   --| . |   |_ -| . | | -_| "
@@ -42,9 +45,11 @@ else
   # Run the API Server and Client Server in parallel processes
 
   # Start the API server on the port above the client
+  echo "Starting API Server..."
   CONFIG_PATH="$CONFIG_PATH" npm run start:server -- -p $(( ${CLIENT_PORT:-3005} + 1)) & pid=$!
   PID_LIST+=" $pid";
   # Start the client server on the port specified
+  echo "Starting Client Server..."
   npm run start:client -- -l ${CLIENT_PORT:-3005} & pid=$!
   PID_LIST+=" $pid";
 
